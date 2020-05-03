d308d5187a0877238d4d9e28f2c23ca125a2cc6d
PadmaGnanapriya <padmaisuru@gmail.com>
2020-05-03 15:01:15 +0530

    Add image into home html

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('base.html')
+def index_page():
+    return render_template('home.html')
+
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+@app.route('/article')
+def article_page():
+    return render_template('article.html')
+
+
+@app.route('/project')
+def project_page():
+    return render_template('project.html')
+
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
